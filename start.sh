16e57f4697d649ed16553c721c0ded11cc7ff9d5
Wayne Lewis <wayne.lewis@esss.se>
2018-10-16 17:02:15 +0000

    Update EVR-1 launch helper scripts to use correct environment

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export IOC_EXEC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-export E3_BIN_DIR="/epics/base-3.15.5/require/3.0.0/bin"
-export IOC_ST_CMD=st.labs-utgard-evr2.cmd
+export VAR_DIR="/opt/nonvolatile/var"
+export E3_BIN_DIR="/home/root/vip/epics/base-3.15.5/require/3.0.0/bin"
+export PROCSERV="/usr/local/bin/procServ"
+export PROCSERV_PORT=2003
+export PROCSERV_IOC_DIR=labs-utgard-evr1
+export IOC_ST_CMD=st.labs-utgard-evr1.cmd
 source "$E3_BIN_DIR/setE3Env.bash"
 $E3_BIN_DIR/iocsh.bash $IOC_EXEC_DIR/$IOC_ST_CMD &
--- start_background.sh
+++ start_background.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export IOC_EXEC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-export VAR_DIR="/var"
-export E3_BIN_DIR="/epics/base-3.15.5/require/3.0.0/bin"
-export PROCSERV="/usr/bin/procServ"
-export PROCSERV_PORT=2000
-export PROCSERV_IOC_DIR=labs-utgard-evr2
-export IOC_ST_CMD=st.labs-utgard-evr2.cmd
+export VAR_DIR="/opt/nonvolatile/var"
+export E3_BIN_DIR="/home/root/vip/epics/base-3.15.5/require/3.0.0/bin"
+export PROCSERV="/usr/local/bin/procServ"
+export PROCSERV_PORT=2006
+export PROCSERV_IOC_DIR=labs-utgard-evr1
+export IOC_ST_CMD=st.labs-utgard-evr1.cmd
 source "$E3_BIN_DIR/setE3Env.bash"
 $PROCSERV -f -L $VAR_DIR/log/procServ/$PROCSERV_IOC_DIR -i ^C^D -c $VAR_DIR/run/procServ/$PROCSERV_IOC_DIR $PROCSERV_PORT $E3_BIN_DIR/iocsh.bash $IOC_EXEC_DIR/$IOC_ST_CMD &
