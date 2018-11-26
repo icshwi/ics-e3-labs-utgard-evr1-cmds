db7ad9c5aff2c12699dbbf9f7e692a0dfb5d91dc
root <nicklas.holmberg@esss.se>
2018-11-26 09:39:25 +0000

    Changed port number from 2003 to 2006

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ export IOC_EXEC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd 
 export VAR_DIR="/opt/nonvolatile/var"
 export E3_BIN_DIR="/home/root/vip/epics/base-3.15.5/require/3.0.0/bin"
 export PROCSERV="/usr/local/bin/procServ"
-export PROCSERV_PORT=2003
+export PROCSERV_PORT=2006
 export PROCSERV_IOC_DIR=labs-utgard-evr1
 export IOC_ST_CMD=st.labs-utgard-evr1.cmd
 source "$E3_BIN_DIR/setE3Env.bash"
